a611c3e13c779a1632c982e7d939ca2abfb5e2d9
manish <bmanish@deaninfotech.com>
2024-02-08 14:29:12 +0530

    update

--- index.php
+++ index.php
@@ -587,7 +587,6 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 
 <script src="assets/js/main.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 <script>
 $(document).ready(function () {
   $("#piramal_form_data_id").submit(function (event) {
